946de8e5b11491f8f0b7e160a8db67894e0d6165
Walid El-Ayoubi <lemirwalid.ayoubi@std.balamand.edu.lb>
2022-09-22 19:42:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+ 
+
+
+
+var services=document.getElementById('Services');
+
+var Original_content= `<div>
+<div class="icon-box">
+  <i class="ri-store-line"></i>
+  <h3><a href="">TITLE</a></h3>
+</div>
+</div>`
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json =>{
+    json.Services.forEach(element => {
+        var content = Original_content;
+        content=content.replace('TITLE',element.A)
+
+        
+        var service = document.createElement('div')
+        service.className='col-xl-2 col-md-4';
+        service.innerHTML = content;
+        services.appendChild(service)
+    });
+  } )
+
+
+
+
