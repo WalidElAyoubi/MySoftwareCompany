8e5158f387e77b68702e5c1b1088f85af72f4151
Walid El-Ayoubi <lemirwalid.ayoubi@std.balamand.edu.lb>
2022-09-28 23:17:50 +0300

    third Trial

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
- 
+var paragraphs=document.getElementById('TextData')
+var services=document.getElementById('Services')
 
-
-
-var services=document.getElementById('Services');
 
 var Original_content= `<div>
 <div class="icon-box">
-  <i class="ri-store-line"></i>
+  <i class="MYICON"></i>
   <h3><a href="">TITLE</a></h3>
 </div>
 </div>`
 
+var Second_content= `<div class="icon-box">
+<div class="icon"><i class="bx bxl-dribbble"></i></div>
+<h4><a href="">Replace2</a></h4>
+<p>Description</p>
+</div>`
 
-fetch('https://localhost:3000/services')
+
+fetch('https://guarded-crag-04182.herokuapp.com/services')
   .then(response => response.json())
   .then(json =>{
     json.Services.forEach(element => {
-        var content = Original_content;
+        
+        var content = Original_content; 
+        var content2= Second_content;//
+        //var content3=Third_content;//
+
+
         content=content.replace('TITLE',element.A)
+        content2=content2.replace('Replace2',element.A)//
+        content2=content2.replace('Description',element.B)//
+        //content3=content3.replace('MYICON',element.C)///
+        ///replace('MYICON',element.C)///
+
+
+        var service = document.createElement('div')
+        var textdata = document.createElement('div')//
+        //var icon = document.createElement('div')//
+
+        service.innerHTML = content;
+        textdata.innerHTML = content2;//
+        //icon.innerHTML = content3;//
+
+
+        service.className='col-xl-2 col-md-4';
+        textdata.className='col-lg-4 col-md-6 d-flex align-items-stretch';//
+        //icon.className='icon';
 
         
-        var service = document.createElement('div')
-        service.className='col-xl-2 col-md-4';
-        service.innerHTML = content;
-        services.appendChild(service)
+        services.appendChild(service);
+        paragraphs.appendChild(textdata);//
     });
   } )
-
 
 
 
